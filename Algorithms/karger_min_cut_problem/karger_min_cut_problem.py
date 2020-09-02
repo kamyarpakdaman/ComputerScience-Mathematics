a26be508ba62cf9e1c61d14708cf0717d29bf48c
Kamyar Pakdaman <57539668+kamyarpakdaman@users.noreply.github.com>
2020-09-03 00:47:49 +0430

    Update karger_min_cut_problem.py

--- Algorithms/karger_min_cut_problem/karger_min_cut_problem.py
+++ Algorithms/karger_min_cut_problem/karger_min_cut_problem.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[229]:
-
+# In this program, we use Karger's randomized algorithm for finding the cut in a given graph with the minimum
+# number of edges.
 
 import numpy as np
 import re
 
-
-# In[232]:
-
+# we use a graph in a dictionary form, where keys are the vertices and the values are list of vertices which
+# form edges with the keys.
 
 def potential_min_cut_finder(g):
     
@@ -41,9 +37,8 @@ def potential_min_cut_finder(g):
     
     return potential_min_cut_finder(g)
 
-
-# In[ ]:
-
+# Since this algorithm is randomized, it doesn't necessarily return the correct answer at the first time; hence,
+# we simply repeat the process a number of times to find out the minimum cut value.
 
 min_cut = None
 

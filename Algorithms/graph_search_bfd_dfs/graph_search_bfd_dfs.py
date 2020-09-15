f0c2bbc0c769199c35025d3663f6964da1cb7f90
Kamyar Pakdaman <57539668+kamyarpakdaman@users.noreply.github.com>
2020-09-16 00:09:48 +0430

    Update graph_search_bfd_dfs.py

--- Algorithms/graph_search_bfd_dfs/graph_search_bfd_dfs.py
+++ Algorithms/graph_search_bfd_dfs/graph_search_bfd_dfs.py
@@ -1,31 +1,56 @@
-# In this program, we will defne functions performing bfs and dfs.
+# In this program, we define two functions performing BFS and DFS algorithms for searching graphs.
+
+# In the Depth-First Search algorithm, we aggressively go through edges like a maze runner.
+
 def dfs(graph, current_vertex, target_value, visited = None):
-      if visited is None:
-    visited = []
+      
+  if visited is None:
+        
+        visited = []
+  
   visited.append(current_vertex)
+  
   if current_vertex is target_value:
-    return visited
+        
+        return visited
   
   for neighbor in graph[current_vertex]:
-    if neighbor not in visited:
-      path = dfs(graph, neighbor, target_value, visited)
-      if path:
-        return path
-      
+        
+        if neighbor not in visited:
+              
+              path = dfs(graph, neighbor, target_value, visited)
+        
+        if path:
+              
+              return path
+
+# In the Breadth-First Search algorithm, we steadily explore all the ways we can go and check them
+# one by one.
+   
 def bfs(graph, start_vertex, target_value):
-  path = [start_vertex]
-  vertex_and_path = [start_vertex, path]
-  bfs_queue = [vertex_and_path]
-  visited = set()
-  while bfs_queue:
-    current_vertex, path = bfs_queue.pop(0)
-    visited.add(current_vertex)
-    for neighbor in graph[current_vertex]:
-      if neighbor not in visited:
-        if neighbor is target_value:
-          return path + [neighbor]
-        else:
-          bfs_queue.append([neighbor, path + [neighbor]])
+      
+      path = [start_vertex]
+      vertex_and_path = [start_vertex, path]
+      bfs_queue = [vertex_and_path]
+      
+      visited = set()
+      
+      while bfs_queue:
+            
+            current_vertex, path = bfs_queue.pop(0)
+            visited.add(current_vertex)
+
+            for neighbor in graph[current_vertex]:
+                  
+                  if neighbor not in visited:
+                        
+                        if neighbor is target_value:
+                              
+                              return path + [neighbor]
+                        
+                        else:
+                              
+                              bfs_queue.append([neighbor, path + [neighbor]])
 
 some_hazardous_graph = {
     'lava': set(['sharks', 'piranhas']),
@@ -37,3 +62,7 @@ some_hazardous_graph = {
 
 print(bfs(some_hazardous_graph, 'sharks', 'bees'))
 print(dfs(some_hazardous_graph, 'sharks', 'bees'))
+
+print('\nThanks for reviewing')
+
+# Thanks for reviewing

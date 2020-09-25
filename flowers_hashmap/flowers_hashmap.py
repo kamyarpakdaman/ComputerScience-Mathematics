765f344257f0e1869a7ce56c220d0f2f5a676371
Kamyar Pakdaman <57539668+kamyarpakdaman@users.noreply.github.com>
2020-09-25 23:34:30 +0330

    Update flowers_hashmap.py

--- flowers_hashmap/flowers_hashmap.py
+++ flowers_hashmap/flowers_hashmap.py
@@ -1,4 +1,4 @@
-# In this project, we are going to implement a hash map to relate the names of flowers to their 
+# In this program, we are going to implement a hash map to relate the names of flowers to their 
 # meanings. In order to avoid collisions when our hashing function collides the names of two flowers, 
 # we are going to use separate chaining. We will implement the Linked List data structure for each
 # of these separate chains.
